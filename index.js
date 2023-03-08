c9d8bbd1fdbd58eb9128615fc1b78a3e2d2be3df
wahid30 <md.wahidulalamjp2@gmail.com>
2023-03-09 02:48:58 +0600

    updated

--- index.js
+++ index.js
@@ -8,82 +8,40 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const animals = [
-//   {
-//     image: "https://ibb.co/1vJ6TXd",
-//     name: "Cookie",
-//     type: "Cat",
-//   },
-//   {
-//     image: "https://ibb.co/9TgVmgX",
-//     name: "Tucker",
-//     type: "Dog",
-//   },
-//   {
-//     image: "https://ibb.co/H7wSSJ2",
-//     name: "Mr.Chukles",
-//     type: "Rabbit",
-//   },
-//   {
-//     image: "https://ibb.co/gJVzCgQ",
-//     name: "Greenie",
-//     type: "Bird",
-//   },
-//   {
-//     image: "https://ibb.co/2PRbJz4",
-//     name: "Mini",
-//     type: "Cat",
-//   },
-//   {
-//     image: "https://ibb.co/x89nhLD",
-//     name: "Buddy",
-//     type: "Dog",
-//   },
-//   {
-//     image: "https://ibb.co/2ktVk1g",
-//     name: "Kookoo",
-//     type: "Bird",
-//   },
-// ];
+userName: vaitasraizel;
+pass: CVBXbWxsmjoMX8nP;
 
-app.get("/adaptAnimals", (req, res) => {
-  res.send(animals);
+const uri =
+  "mongodb+srv://vaitasraizel:CVBXbWxsmjoMX8nP@cluster0.cpadwda.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
-// userName: vaitasraizel
-// pass: CVBXbWxsmjoMX8nP
+async function run() {
+  try {
+    const animalAdoptions = client
+      .db("animalAdoptions")
+      .collection("adaptions");
 
-// const uri =
-//   "mongodb+srv://vaitasraizel:CVBXbWxsmjoMX8nP@cluster0.cpadwda.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   serverApi: ServerApiVersion.v1,
-// });
+    app.get("/adaptAnimals", async (req, res) => {
+      const cursor = animalAdoptions.find({});
+      const animals = await cursor.toArray();
+      res.send(animals);
+    });
 
-// async function run() {
-//   try {
-//     const animalAdoptions = client
-//       .db("animalAdoptions")
-//       .collection("adaptions");
-
-//     app.get("/adaptAnimals", async (req, res) => {
-//       const cursor = animalAdoptions.find({});
-//       const animals = await cursor.toArray();
-//       res.send(animals);
-//     });
-
-//     app.post("/adaptAnimals", async (req, res) => {
-//       console.log("post api called");
-//       const animals = req.body;
-//       const result = await animalAdoptions.insertOne(animals);
-//       animals.id = result.insertedId;
-//       res.send(animals);
-//     });
-//   } finally {
-//   }
-// }
-// run().catch((err) => console.log(err));
+    app.post("/adaptAnimals", async (req, res) => {
+      console.log("post api called");
+      const animals = req.body;
+      const result = await animalAdoptions.insertOne(animals);
+      animals.id = result.insertedId;
+      res.send(animals);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
